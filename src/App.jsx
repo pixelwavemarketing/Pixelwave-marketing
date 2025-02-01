82fa410fc3183747f7aabfeec0917d1fecadc4b9
Dylan Milavec <dylanmilavec@Dylans-MacBook-Pro.local>
2025-02-01 18:07:40 -0500

    Added Netlify redirects and fixed routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
 import Home from './pages/Home'
 import Services from './pages/Services'
 import Contact from './pages/Contact'
@@ -7,19 +6,11 @@ import Contact from './pages/Contact'
 function App() {
   return (
     <Router>
-      <div style={{ 
-        margin: 0, 
-        paddingTop: '60px',
-        backgroundColor: '#ffffff',
-        minHeight: '100vh'
-      }}>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </Router>
   )
 }
